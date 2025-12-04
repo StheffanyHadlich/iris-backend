33e2d3a7e0339558eb3b92a46edc7a325b0e783b
RUYvo <engs-ruyjunior@camporeal.edu.br>
2025-12-04 15:01:46 -0300

    fix: add PetsModule import to DailyModule

--- src/daily/daily.module.ts
+++ src/daily/daily.module.ts
@@ -1,11 +1,13 @@
+// src/daily/daily.module.ts
 import { Module } from '@nestjs/common';
 import { DiaryService } from './service/daily.service';
 import { DiaryController } from './controller/daily.controller';
 import { DiaryRepository } from './repository/diary.repository';
 import { PrismaModule } from '../prisma.module';
+import { PetsModule } from '../pets/pets.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, PetsModule],
   providers: [DiaryService, DiaryRepository],
   controllers: [DiaryController],
   exports: [DiaryService, DiaryRepository],
